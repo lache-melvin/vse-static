837a4fb8c3407cb1dff8afec0a44f40a8856651e
Lache Melvin <lache.melvin@gmail.com>
2020-12-21 18:55:48 +1300

    add new images

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ class RootIndex extends React.Component {
           <Helmet title={siteTitle} />
           <div className="wrapper">
             <div className="home-title-image row">
-              <h1 className="home-title">Vector Structural Engineering</h1>
-              <div style={{ height: '250px' }}></div>
+              {/* <h1 className="home-title">Vector Structural Engineering</h1> */}
+              <img src={assets.teamFront} alt="VSE team outside the front of the office on Rutherford Street" />
             </div>
             <h2 >Full website coming soon...</h2>
           </div>
