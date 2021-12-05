db7149497b05cb1055a1912a2034f11991a94502
Lache Melvin <lache.melvin@gmail.com>
2021-12-05 13:08:02 +1300

    update deps

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import './base.css'
-import Container from './container'
-import Navigation from './navigation'
+import React from "react";
+import "./base.css";
+import Container from "./container";
+import Navigation from "./navigation";
 
 class Template extends React.Component {
   render() {
-    const { children, assets, location } = this.props
+    const { children, assets, location } = this.props;
     return (
       <Container>
-        <div className='header'>
+        <div className="header">
           <div className="row">
             <div className="header-logo-div">
-              <img className="header-logo" src={assets.logo} alt='VSE Logo' />
+              <img className="header-logo" src={assets.logo} alt="VSE Logo" />
             </div>
             <Navigation location={location} />
           </div>
         </div>
         {children}
-        <div className='footer'>
-          <div className='row'>
-            <img src={assets.engNZ} alt='Engineering New Zealand logo' />
+        <div className="footer">
+          <div className="row">
+            <img src={assets.engNZ} alt="Engineering New Zealand logo" />
           </div>
         </div>
       </Container>
-    )
+    );
   }
 }
 
-export default Template
+export default Template;
