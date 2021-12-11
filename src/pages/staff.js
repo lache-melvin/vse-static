38b7810cd0dc0c2ca97e3e394fc23981ec3df4de
Lache Melvin <lache.melvin@gmail.com>
2021-12-12 12:48:25 +1300

    update header and nav (add burger) for mobile

--- src/pages/staff.js
+++ src/pages/staff.js
@@ -1,44 +1,47 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
-// import styles from './blog.module.css'
-import Layout from '../components/layout'
-import StaffMember from '../components/staff-member'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
+import StaffMember from "../components/staff-member";
 
 class StaffIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const staff = get(this, 'props.data.allContentfulStaffMember.edges')
-    const assets = get(this, 'props.data.allContentfulAsset.edges')
-      .reduce((acc, { node }) => {
-        acc[node.title] = node.file.url
-        return acc
-      }, {})
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const staff = get(this, "props.data.allContentfulStaffMember.edges");
+    const assets = get(this, "props.data.allContentfulAsset.edges").reduce(
+      (acc, { node }) => {
+        acc[node.title] = node.file.url;
+        return acc;
+      },
+      {}
+    );
 
     return (
       <Layout assets={assets} location={this.props.location}>
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: "#fff" }}>
           <Helmet title={siteTitle} />
           <div className="wrapper">
-            <h2 className="section-headline" style={{paddingBottom: '3vmin'}}>Our Team</h2>
-            <div className='staff'>
-              {staff.map(({ node }) => <StaffMember key={node.name} member={node} />)}
+            <h2 className="section-headline" style={{ paddingBottom: "3vmin" }}>
+              Our Team
+            </h2>
+            <div className="staff">
+              {staff.map(({ node }) => (
+                <StaffMember key={node.name} member={node} />
+              ))}
             </div>
           </div>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-// className="article-list" <<=== for the ul tag
-
-export default StaffIndex
+export default StaffIndex;
 
 export const pageQuery = graphql`
-  query StaffQuery  {
-    allContentfulStaffMember(sort: {fields: sortId, order: ASC}) {
+  query StaffQuery {
+    allContentfulStaffMember(sort: { fields: sortId, order: ASC }) {
       edges {
         node {
           name
@@ -66,4 +69,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
