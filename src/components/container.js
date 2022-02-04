c39b2c69e079388d373bf1cbc74000c287368441
Lache Melvin <lache.melvin@gmail.com>
2022-02-05 11:40:39 +1300

    wip header

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navigation from "./navigation";
 
-export default ({ children }) => (
-  <div style={{ maxWidth: 1180, margin: '0 auto' }}>{children}</div>
-)
+export default ({ children, location, logo }) => (
+  <>
+    <Navigation location={location} />
+    <div className="header">
+      <div className="header-logo-div">
+        <img className="header-logo" src={logo} alt="VSE Logo" />
+      </div>
+    </div>
+    <div style={{ maxWidth: 1180, margin: "0 auto", top: "100px" }}>
+      {children}
+    </div>
+  </>
+);
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./base.css";
 import Container from "./container";
-import Navigation from "./navigation";
 
 class Template extends React.Component {
   render() {
     const { children, assets, location } = this.props;
     return (
-      <Container>
-        <div className="header">
-          <div className="header-logo-div">
-            <img className="header-logo" src={assets.logo} alt="VSE Logo" />
-          </div>
-          <Navigation location={location} />
-        </div>
+      <Container location={location} logo={assets.logo}>
         {children}
         <div className="footer">
           <div className="row">
