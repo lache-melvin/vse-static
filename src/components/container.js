a14139c56845e5d40d7f070aed3ea48e86279fe7
Lache Melvin <lache.melvin@gmail.com>
2022-07-11 21:04:31 +1200

    make logo a home link

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "gatsby";
 import Navigation from "./navigation";
 
 const Container = ({ children, location, logo }) => (
   <>
     <Navigation location={location} />
     <div className="header">
-      <div className="header-logo-div">
-        <img className="header-logo" src={logo} alt="VSE Logo" />
-      </div>
+      <Link to="/">
+        <div className="header-logo-div">
+          <img className="header-logo" src={logo} alt="VSE Logo" />
+        </div>
+      </Link>
     </div>
     <div style={{ maxWidth: 1180, margin: "0 auto", top: "12vh", position: "relative" }}>
       {children}
